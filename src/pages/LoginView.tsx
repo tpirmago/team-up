844a62136b23f001a745431c6ec2a3d892f4cfc2
johannaranki <jranki24@students.oamk.fi>
2026-03-24 22:00:09 +0200

    Clean up default files and add project-related folders and LoginView

---
+++ src/pages/LoginView.tsx
@@ -0,0 +1,19 @@
+export default function LoginView() {
+    return (
+        <div >
+            <text>Log in to TeamUp</text>
+            <div>
+                <label>Email</label>
+                <input></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password"></input>
+            </div>
+            <button>Log in</button>
+            <text>Not a member yet? <a href="#">Sign up here</a></text>
+        </div>
+    )
+}
+
+
