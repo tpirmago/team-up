a1b7e2202fe11a198710440c490812a18968429d
Tatiana Pirmagomedova <ptju.spb@gmail.com>
2026-04-15 10:57:15 +0300

    frontend/tatiana: add dashboard view

---
+++ frontend/src/components/Footer 2.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <span>© 2026 TeamUp</span>
+        </footer>
+    )
+}
